edcac190da2599d7740edaaa165287ec400c0582
Raul12347 <raul79941@gmail.com>
2025-05-29 20:04:20 -0600

    Se acomplento la base de datos

--- conexion.php
+++ conexion.php
@@ -1,5 +1,11 @@
 <?php 
-$usuario = $root;
-$contra = $root;
+$servidor="localhots";
+$base = "tareas_ges";
+$username = "root";
+$pass = "";
+$conn = mysqli_connect($servidor,$username,$pass) or die ("No se a podido conectar el servidor");
+$db = mysqli_select_db($conn,$base) or die("UPPS!  Error a conectar a la base de datos");
+//verificar la conexin
+
 
 ?>
